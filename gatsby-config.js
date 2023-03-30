64c513cf11f41f262c70b974468071e29c9eba1a
Jack Robetson <jackrobertsonps3@gmail.com>
2023-03-30 18:30:47 +0100

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `jack_robertsons-website`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-netlify-cms", "gatsby-plugin-styled-components"]
+};
